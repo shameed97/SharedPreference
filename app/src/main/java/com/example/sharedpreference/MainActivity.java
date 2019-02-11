15fb83e5c032a96b829478a2df15fca7cb55aadf
shame <shameeed97.pno@gmailcom>
2019-02-11 09:37:37 +0530

    initial commit
    toast added

--- app/src/main/java/com/example/sharedpreference/MainActivity.java
+++ app/src/main/java/com/example/sharedpreference/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Toast.makeText(this, "Hello toast", Toast.LENGTH_SHORT).show();
-        Toast.makeText(this, "Hello toast", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Hello dev", Toast.LENGTH_SHORT).show();
 
         PreferencesConfig=new SharedPreferencesConfig(getApplicationContext());
 
