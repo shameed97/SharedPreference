31b93245eac31239b0f37729c2dd19e4db231829
shame <shameeed97.pno@gmailcom>
2019-02-11 09:36:24 +0530

    initial commit
    toast added

--- app/src/main/java/com/example/sharedpreference/MainActivity.java
+++ app/src/main/java/com/example/sharedpreference/MainActivity.java
@@ -8,7 +8,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText username,password;
+    private EditText username, password;
     private SharedPreferencesConfig PreferencesConfig;
 
     @Override
@@ -18,27 +18,27 @@ public class MainActivity extends AppCompatActivity {
 
         Toast.makeText(this, "Hello toast", Toast.LENGTH_SHORT).show();
 
-        PreferencesConfig=new SharedPreferencesConfig(getApplicationContext());
+        PreferencesConfig = new SharedPreferencesConfig(getApplicationContext());
 
-        username=findViewById(R.id.edit_1);
-        password=findViewById(R.id.edit_2);
+        username = findViewById(R.id.edit_1);
+        password = findViewById(R.id.edit_2);
 
-        if (PreferencesConfig.readLoginStatus()){
-            startActivity(new Intent(this,SuccessActivity.class));
+        if (PreferencesConfig.readLoginStatus()) {
+            startActivity(new Intent(this, SuccessActivity.class));
             finish();
         }
     }
 
     public void login(View view) {
-        String Username=username.getText().toString();
-        String Password=password.getText().toString();
-        if (Username.equals(getResources().getString(R.string.User_name))&&Password.equals(getResources().getString(R.string.Pass_word))){
-            startActivity(new Intent(this,SuccessActivity.class));
-        PreferencesConfig.writeLoginStatus(true);
-        finish();
-        }
-        else{
-            Toast.makeText(this,"Login failed...Try again",Toast.LENGTH_SHORT).show();;
+        String Username = username.getText().toString();
+        String Password = password.getText().toString();
+        if (Username.equals(getResources().getString(R.string.User_name)) && Password.equals(getResources().getString(R.string.Pass_word))) {
+            startActivity(new Intent(this, SuccessActivity.class));
+            PreferencesConfig.writeLoginStatus(true);
+            finish();
+        } else {
+            Toast.makeText(this, "Login failed...Try again", Toast.LENGTH_SHORT).show();
+            ;
             username.setText("");
             password.setText("");
         }
