3abe4ec5395be4edc22efc890f7fa1ddbac13cfc
shame <shameeed97.pno@gmailcom>
2019-02-11 09:23:52 +0530

    initial commit

--- app/src/main/java/com/example/sharedpreference/SharedPreferencesConfig.java
+++ app/src/main/java/com/example/sharedpreference/SharedPreferencesConfig.java
@@ -8,19 +8,21 @@ public class SharedPreferencesConfig {
     private SharedPreferences sharedPreferences;
     private Context context;
 
-    public SharedPreferencesConfig(Context context){
-        this.context=context;
-        sharedPreferences=context.getSharedPreferences(context.getResources().getString(R.string.login_status),Context.MODE_PRIVATE);
+    public SharedPreferencesConfig(Context context) {
+        this.context = context;
+        sharedPreferences = context.getSharedPreferences(context.getResources().getString(R.string.login_status), Context.MODE_PRIVATE);
 
     }
-    public void writeLoginStatus(Boolean status){
-        SharedPreferences.Editor editor=sharedPreferences.edit();
-    editor.putBoolean(context.getResources().getString(R.string.Login_preferences),status);
-    editor.commit();
+
+    public void writeLoginStatus(Boolean status) {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean(context.getResources().getString(R.string.Login_preferences), status);
+        editor.commit();
     }
-    public boolean readLoginStatus(){
-        boolean status=false;
-       status=sharedPreferences.getBoolean(context.getResources().getString(R.string.Login_preferences),false);
+
+    public boolean readLoginStatus() {
+        boolean status = false;
+        status = sharedPreferences.getBoolean(context.getResources().getString(R.string.Login_preferences), false);
         return status;
     }
 }
