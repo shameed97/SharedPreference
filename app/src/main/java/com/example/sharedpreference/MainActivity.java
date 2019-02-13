2b25c3a9530b1763780c940a13a07d784cbeb303
shame <shameeed97.pno@gmailcom>
2019-02-13 09:19:50 +0530

    login form using sharedpreferences

--- app/src/main/java/com/example/sharedpreference/MainActivity.java
+++ app/src/main/java/com/example/sharedpreference/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.sharedpreference;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -37,7 +37,6 @@ public class MainActivity extends AppCompatActivity {
             finish();
         } else {
             Toast.makeText(this, "Login failed...Try again", Toast.LENGTH_SHORT).show();
-            ;
             username.setText("");
             password.setText("");
         }
